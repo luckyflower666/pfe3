abf008d61feac49a99b1ead330364c30f317e951
caocasey <casey@caseytekiMacBook-Pro.local>
2013-11-22 18:23:25 +0100

    add the index link page without create function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pfe2::Application.routes.draw do
+  get "welcome/index"
   resources :stores
 
   resources :owners
@@ -21,7 +22,7 @@ Pfe2::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
